15f859566915fd5459ab950895044b3b0304ea25
Revanth Raghu <revabob@gmail.com>
2020-10-10 22:51:41 +0530

    changed error response

--- frontend/src/Redux/actions.js
+++ frontend/src/Redux/actions.js
@@ -1,67 +1,67 @@
 import {
-  POST_LOGIN_REQUEST,
-  POST_LOGIN_SUCCESS,
-  POST_LOGIN_FAILURE,
-  POST_REGISTER_REQUEST,
-  POST_REGISTER_SUCCESS,
-  POST_REGISTER_FAILURE,
-} from "./actionTypes";
-import axios from "axios";
+ POST_LOGIN_REQUEST,
+ POST_LOGIN_SUCCESS,
+ POST_LOGIN_FAILURE,
+ POST_REGISTER_REQUEST,
+ POST_REGISTER_SUCCESS,
+ POST_REGISTER_FAILURE
+} from './actionTypes';
+import axios from 'axios';
 
 // register requests
 export const postRegisterRequest = (payload) => ({
-  type: POST_REGISTER_REQUEST,
-  payload,
+ type: POST_REGISTER_REQUEST,
+ payload
 });
 
 export const postRegisterSuccess = (payload) => ({
-  type: POST_REGISTER_SUCCESS,
-  payload,
+ type: POST_REGISTER_SUCCESS,
+ payload
 });
 
 export const postRegisterFailure = (payload) => ({
-  type: POST_REGISTER_FAILURE,
-  payload,
+ type: POST_REGISTER_FAILURE,
+ payload
 });
 
 export const postRegister = (payload) => {
-  return (dispatch) => {
-    return axios({
-      method: "post",
-      url: "http://localhost:5000/api/register",
-      data: payload,
-      headers: { "content-type": "application/json" },
-    })
-      .then((res) => dispatch(postRegisterSuccess(res.data)))
-      .catch((err) => dispatch(postRegisterFailure(err)));
-  };
+ return (dispatch) => {
+  return axios({
+   method: 'post',
+   url: 'http://localhost:5000/api/register',
+   data: payload,
+   headers: { 'content-type': 'application/json' }
+  })
+   .then((res) => dispatch(postRegisterSuccess(res.data)))
+   .catch((err) => dispatch(postRegisterFailure(err.response.data)));
+ };
 };
 
 // login requests
 export const postLoginRequest = (payload) => ({
-  type: POST_LOGIN_REQUEST,
-  payload,
+ type: POST_LOGIN_REQUEST,
+ payload
 });
 
 export const postLoginSuccess = (payload) => ({
-  type: POST_LOGIN_SUCCESS,
-  payload,
+ type: POST_LOGIN_SUCCESS,
+ payload
 });
 
 export const postLoginFailure = (payload) => ({
-  type: POST_LOGIN_FAILURE,
-  payload,
+ type: POST_LOGIN_FAILURE,
+ payload
 });
 
 export const postLogin = (payload) => (dispatch) => {
-  console.log(payload);
-  dispatch(postLoginRequest(payload));
-  axios({
-    method: "post",
-    url: "http://localhost:5000/api/login",
-    headers: { "content-type": "application/json" },
-    data: payload,
-  })
-    .then((res) => dispatch(postLoginSuccess(res.data)))
-    .catch((err) => dispatch(postLoginFailure(err)));
+ console.log(payload);
+ dispatch(postLoginRequest(payload));
+ axios({
+  method: 'post',
+  url: 'http://localhost:5000/api/login',
+  headers: { 'content-type': 'application/json' },
+  data: payload
+ })
+  .then((res) => dispatch(postLoginSuccess(res.data)))
+  .catch((err) => dispatch(postLoginFailure(err.response.data)));
 };
--- frontend/src/Redux/reducer.js
+++ frontend/src/Redux/reducer.js
@@ -1,60 +1,71 @@
-import {POST_REGISTER_REQUEST,POST_REGISTER_SUCCESS,POST_REGISTER_FAILURE,POST_LOGIN_REQUEST,POST_LOGIN_SUCCESS,POST_LOGIN_FAILURE} from './actionTypes'
-import {loadData,saveData} from './localStorage'
+import {
+ POST_REGISTER_REQUEST,
+ POST_REGISTER_SUCCESS,
+ POST_REGISTER_FAILURE,
+ POST_LOGIN_REQUEST,
+ POST_LOGIN_SUCCESS,
+ POST_LOGIN_FAILURE
+} from './actionTypes';
+import { loadData, saveData } from './localStorage';
 
 export const initialState = {
-    isLoading:false,
-    isError:false,
-    userData:[],
-    login: loadData("expenseManagerAuth") || false,
-}
+ isLoading: false,
+ isError: false,
+ userData: [],
+ login: loadData('expenseManagerAuth') || false,
+ message: ''
+};
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case POST_REGISTER_REQUEST:
-            return {
-                ...state,
-                isLoading:true,
-                isError:false
-            }
-            
-        case POST_REGISTER_SUCCESS:
-            return {
-                ...state,
-                isLoading:false,
-                isError:false
-            }
+ switch (action.type) {
+  case POST_REGISTER_REQUEST:
+   return {
+    ...state,
+    isLoading: true,
+    isError: false
+   };
 
-        case POST_REGISTER_FAILURE:
-            return {
-                ...state,
-                isLoading:false,
-                isError:true
-            }
-        case POST_LOGIN_REQUEST:
-            return {
-                ...state,
-                isLoading:true,
-                isError:false
-            }
-            
-        case POST_LOGIN_SUCCESS:
-            saveData('expenseManagerAuth',true)
-            return {
-                ...state,
-                isLoading:false,
-                isError:false,
-                login:true
-            }
+  case POST_REGISTER_SUCCESS:
+   return {
+    ...state,
+    isLoading: false,
+    isError: false
+   };
 
-        case POST_LOGIN_FAILURE:
-            console.log(action.payload);
-            return {
-                ...state,
-                isLoading:false,
-                isError:true
-            }
+  case POST_REGISTER_FAILURE:
+   console.log(action.payload);
+   return {
+    ...state,
+    isLoading: false,
+    isError: true,
+    message: action.payload
+   };
+  case POST_LOGIN_REQUEST:
+   return {
+    ...state,
+    isLoading: true,
+    isError: false
+   };
 
-        default:
-            return state
-    }
-}
+  case POST_LOGIN_SUCCESS:
+   saveData('expenseManagerAuth', true);
+   return {
+    ...state,
+    isLoading: false,
+    isError: false,
+    login: true
+   };
+
+  case POST_LOGIN_FAILURE:
+   console.log(action.payload);
+   return {
+    ...state,
+    isLoading: false,
+    isError: true,
+    message: action.payload
+   };
+
+  default:
+   return state;
+ }
+};
