7237aa16d0fa9eb22e21c37602f392f262597710
Revanth Raghu <revabob@gmail.com>
2020-10-14 15:59:16 +0530

    added background video and adjusted component styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,20 @@ import Login from './Components/Login';
 import Register from './Components/Register';
 
 function App() {
-  return (
-    <div className="App">
-      <Route path='/' exact component={Register}/>
-      <Route path = '/login' component={Login}/>
+ return (
+  <div className="App">
+   <div>
+    <video autoPlay muted loop className="video">
+     <source src="bg.mp4" type="video/mp4" />
+    </video>
+    <h1 className="heading">Expense Manager</h1>
+    <div className="card">
+     <Route path="/" exact component={Register} />
+     <Route path="/login" component={Login} />
     </div>
-  );
+   </div>
+  </div>
+ );
 }
 
 export default App;
--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,85 +1,90 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import TextField from "@material-ui/core/TextField";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import EmailIcon from "@material-ui/icons/Email";
-import NavBar from "./NavBar";
-import {postLogin} from '../Redux/actions'
-import { Redirect } from "react-router-dom";
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import EmailIcon from '@material-ui/icons/Email';
+import NavBar from './NavBar';
+import { postLogin } from '../Redux/actions';
+import { Redirect } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& .MuiTextField-root": {
-      margin: theme.spacing(1),
-      width: 200,
-    },
-  },
+ root: {
+  background: 'white',
+  width: 307.5,
+  borderRadius: '0 0 8px 8px',
+  flexGrow: 1,
+  marginLeft: '75%',
+  '& .MuiTextField-root': {
+   margin: theme.spacing(1)
+  }
+ }
 }));
 
 export default function Login() {
-  const classes = useStyles();
+ const classes = useStyles();
 
-  const initialState = {
-    email: "",
-    password: "",
-  };
+ const initialState = {
+  email: '',
+  password: ''
+ };
 
-  const [loginDetails, setLoginDetails] = useState(initialState);
-  const login = useSelector(state => state.login)
-  const dispatch = useDispatch();
+ const [loginDetails, setLoginDetails] = useState(initialState);
+ const login = useSelector((state) => state.login);
+ const dispatch = useDispatch();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setLoginDetails((state) => ({ ...state, [name]: value }));
-  };
+ const handleChange = (e) => {
+  const { name, value } = e.target;
+  setLoginDetails((state) => ({ ...state, [name]: value }));
+ };
 
-  const handleLogin = () => {
-    dispatch(postLogin(loginDetails))
-  };
+ const handleLogin = () => {
+  dispatch(postLogin(loginDetails));
+ };
 
-  return (
+ return (
+  <>
+   {login ? (
+    <Redirect to="/dashboard" />
+   ) : (
     <>
-      {
-        login ? <Redirect to = '/dashboard'/> : 
-      <>
-      <NavBar />
-      <form
-        className={classes.root}
-        style={{ marginLeft: "60%" }}
-        noValidate
-        autoComplete="off"
-      >
-        <div>
-          <TextField
-            type="email"
-            name="email"
-            start={<EmailIcon />}
-            onChange={handleChange}
-            value={loginDetails.email}
-            error
-            id="standard-basic"
-            label="Email"
-          />
-        </div>
-        <div>
-          <TextField
-            name="password"
-            onChange={handleChange}
-            value={loginDetails.password}
-            error
-            id="standard-basic"
-            label="Password"
-          />
-        </div>
-        <div>
-          <Button onClick={handleLogin} variant="contained" color="secondary">
-            Login
-          </Button>
-        </div>
-      </form>
-      </>
-      }
+     <NavBar />
+     <form className={classes.root} noValidate autoComplete="off">
+      <div>
+       <TextField
+        type="email"
+        name="email"
+        start={<EmailIcon />}
+        onChange={handleChange}
+        value={loginDetails.email}
+        error
+        id="standard-basic"
+        label="Email"
+       />
+      </div>
+      <div>
+       <TextField
+        name="password"
+        onChange={handleChange}
+        value={loginDetails.password}
+        error
+        id="standard-basic"
+        label="Password"
+       />
+      </div>
+      <div>
+       <Button
+        style={{ width: '80%', margin: '30px' }}
+        onClick={handleLogin}
+        variant="contained"
+        color="secondary"
+       >
+        Login
+       </Button>
+      </div>
+     </form>
     </>
-  );
+   )}
+  </>
+ );
 }
